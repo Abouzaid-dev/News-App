0993a895fe5625b1fc979d9347b852d4c03e6fe1
Mahmoud Abouzaid <m.abouzaid.flutter.dev@gmail.com>
2025-04-07 17:45:44 +0200

    services

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/views/home_view.dart';
 
 void main(){
+  getNews();
   runApp(NewsApp());
 }
+
+final dio = Dio();
+
+void getNews() async {
+  final response = await dio.get('https://newsapi.org/v2/everything?q=bitcoin&apiKey=6857bffbde1c41e79a2cd8621e854a3a');
+  print(response);
+  
+}
+// The main widget of the application.
 
 class NewsApp extends StatelessWidget {
   const NewsApp({super.key});
