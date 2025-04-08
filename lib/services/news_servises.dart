e0ed7ec4ea1b6e8d29e75914dda0048431bf391c
Mahmoud Abouzaid <m.abouzaid.flutter.dev@gmail.com>
2025-04-08 17:42:34 +0200

    SF

--- lib/services/news_servises.dart
+++ lib/services/news_servises.dart
@@ -1,7 +1,9 @@
-
+import 'package:dio/dio.dart';
 
 class NewsServises {
+  final Dio dio = Dio();
 
-  getGeneral(){}
-
+  getGeneralNews() async {
+    Response response = await dio.get("https://newsdata.io/api/1/latest?apikey=pub_79152092a576295542aaffc99380ab581b2d8&country=eg&category=top");
+  }
 }
